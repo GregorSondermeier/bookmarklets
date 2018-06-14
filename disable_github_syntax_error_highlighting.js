daeda431dcb81b599a508e5acdaddf13aa2832e2
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2018-06-14 09:40:30 +0200

    white spacing

--- disable_github_syntax_error_highlighting.js
+++ disable_github_syntax_error_highlighting.js
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // on a github pull request, this removes the red background / white font color for syntax errors //
-//											!ES2016!											  //
+//                                              !ES2016!                                          //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 (function() {for (var el of document.getElementsByClassName("pl-ii")) {
 	el.style.backgroundColor = "transparent";
