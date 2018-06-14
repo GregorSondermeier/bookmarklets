7fa4db2941d8b5aeae3e04b7bcd7b7f88a6c56c0
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2018-06-14 09:37:49 +0200

    make last commit usable as bookmarklet

--- disable_github_syntax_error_highlighting.js
+++ disable_github_syntax_error_highlighting.js
@@ -2,7 +2,7 @@
 // on a github pull request, this removes the red background / white font color for syntax errors //
 //											!ES2016!											  //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-for (var el of document.getElementsByClassName("pl-ii")) {
+(function() {for (var el of document.getElementsByClassName("pl-ii")) {
 	el.style.backgroundColor = "transparent";
 	el.style.color = "#24292e";
-}
+}})();
